a00344322b7a2fb84f5d5ed7179a07dd5d738e04
jgutman <jacqueline.gutman@gmail.com>
2016-05-04 19:28:01 -0400

    some tweaks, begin adding debate.df

--- debate/FinalProject.R
+++ debate/FinalProject.R
@@ -22,10 +22,13 @@ twitter$sentiment <- as.factor(twitter$sentiment)
 twitter$subject_matter <- as.factor(twitter$subject_matter)
 # In creating these timestamps, we discarded timezone data. Worth it to include?
 twitter$tweet_created <- strptime(twitter$tweet_created, "%m/%d/%y %H:%M")
+twitter$candidate <- mapvalues(twitter$candidate, from=c("", "No candidate mentioned"), to=c("OTHER", "OTHER"))
+twitter$subject_matter <- mapvalues(twitter$subject_matter, from=c(""), to=c("None of the above"))
 View(twitter)
 
 # Create quanteda corpus object for twitter data with appropriate metadata
-twitter_corpus <- corpus(twitter$text, docvars = twitter[,-5])
+text_col <- which(colnames(twitter) == "text")
+twitter_corpus <- corpus(twitter$text, docvars = twitter[,-text_col])
 colnames(twitter_corpus$documents)
 # DFM: remove # symbol, @ symbol, RT, English stopwords, numbers, punctuation
 # convert all words to lowercase
@@ -51,10 +54,23 @@ best_k <- possible_k[which.min(model$results$residual)]
 # Warning: this takes a REALLY long time to run!
 twitter_34 <- stm(out_stm$documents, out_stm$vocab, K = best_k, init.type="Spectral", 
                  content = ~ subject_matter, 
-                 prevalence = ~ candidate + subject_matter + as.numeric(tweet_created) + retweet_count, 
+                 prevalence = ~ candidate + subject_matter + as.numeric(tweet_created), 
                  max.em.its=30, data=out_stm$meta, seed=1100)
 
 # Save the current workspace to disk
 save.image("~/Desktop/Text as Data/texas/debate/project_jackie.RData")
 # Load the saved workspace from disk
 load("~/Desktop/Text as Data/texas/debate/project_jackie.RData")
+
+source("./parse_debate.R")
+debate.df$snippets <- as.character(debate.df$snippets)
+levels(debate.df$speakers)
+levels(twitter$candidate)
+
+debate.df$speakers <- revalue(debate.df$speakers, c("(UNKNOWN)"="OTHER", "BAIER"="MODERATOR", "BUSH"="Jeb Bush", 
+            "CARSON"="Ben Carson", "MEGAN"="MODERATOR", "PAUL"="Rand Paul",
+            "CHRISTIE"="Chris Christie", "COMMERCIAL"="OTHER", "CRUZ"="Ted Cruz", "FIORINA"="OTHER", 
+            "HUCKABEE"="Mike Huckabee", "KASICH"="John Kasich", "KELLY"="MODERATOR", "MALE"="OTHER",
+            "PERRY"="OTHER", "QUESTION"="OTHER", "RUBIO"="Marco Rubio", "TRUMP"="Donald Trump",
+            "UNIDENTIFIED FEMALE"="OTHER", "UNIDENTIFIED MALE"="OTHER", "WALKER"="Scott Walker",
+            "WALKRE"="Scott Walker", "WALLACE"="MODERATOR", "WALLCE"="MODERATOR"))
